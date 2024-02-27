76f0d2dec7c9512d2000d944e6fec7e64215d911
lyfunltd <lyfunltd@gmail.com>
2024-02-27 13:16:43 +0200

    first build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test app to see if I can push to the cloud
         </p>
         <a
           className="App-link"
